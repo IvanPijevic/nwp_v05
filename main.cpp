08445a1a35ee046f01253231dee6f4c72d03bee5
Ivan Pijević <ivpijevic@vsite.hr>
2025-04-14 18:13:03 +0200

    Paint

--- main.cpp
+++ main.cpp
@@ -1,32 +1,75 @@
 #include <tchar.h>
+#include <list>
+
 #include "nwpwin.h"
+
 
 class main_window : public vsite::nwp::window
 {
+private:
+    std::list<POINT> points;
+
 protected:
-	void on_paint(HDC hdc) override  
-	{ 
-	// TODO: iterate over points in container and draw polyline
-	}
-	void on_left_button_down(POINT p) override
-	{
-	// TODO: add point to container
-	}
-	void on_key_down(int vk) override
-	{
-	// TODO: Esc - empty container
-	// TODO: Backspace - remove last point
-	}
-	void on_destroy() override
-	{
-		::PostQuitMessage(0);
-	}
+    void on_paint(HDC hdc) override
+    {
+        if (points.size() >= 2) 
+        {
+            auto it = points.begin();
+            MoveToEx(hdc, it->x, it->y, nullptr);
+            ++it;
+
+            while (it != points.end()) 
+            {
+                LineTo(hdc, it->x, it->y);
+                ++it;
+            }
+        }
+    }
+
+    void on_left_button_down(POINT p) override
+    {
+        points.emplace_back(p);
+        InvalidateRect(*this, nullptr, TRUE);
+    }
+
+    void on_key_down(int vk) override
+    {
+        bool changed = false;
+
+        switch (vk) 
+        {
+        case VK_ESCAPE:
+            if (!points.empty()) 
+            {
+                points.clear();
+                changed = true;
+            }
+            break;
+
+        case VK_BACK:
+            if (!points.empty()) 
+            {
+                points.pop_back();
+                changed = true;
+            }
+            break;
+        }
+        if (changed) 
+        {
+            InvalidateRect(*this, nullptr, TRUE);
+        }
+    }
+
+    void on_destroy() override
+    {
+        ::PostQuitMessage(0);
+    }
 };
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
-	vsite::nwp::application app;
-	main_window w;
-	w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 5");
-	return app.run();
+    vsite::nwp::application app;
+    main_window w;
+    w.create(0, WS_OVERLAPPEDWINDOW | WS_VISIBLE, "NWP 5");
+    return app.run();
 }
